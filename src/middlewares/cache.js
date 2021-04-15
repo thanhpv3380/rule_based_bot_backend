baf1ddb9d0f57be0471e47a6dc23a60cc6f9664e
Henry <thanhpv3380@gmail.com>
2021-04-16 02:01:30 +0700

    change type entity to number

--- src/middlewares/cache.js
+++ src/middlewares/cache.js
@@ -1,28 +1,28 @@
-const redisClient = require('redis');
-const asyncMiddleware = require('./async');
-const chatbotService = require('../services/chatbot');
+// const redisClient = require('redis');
+// const asyncMiddleware = require('./async');
+// const chatbotService = require('../services/chatbot');
 
-const client = redisClient.createClient(6379);
+// const client = redisClient.createClient(6379);
 
-const checkCacheUsersay = (req, res, next) => {
-  const { bot } = req;
-  const { usersay } = req.query;
-  // client.lpushAsync();
-  client.get(bot.id, async (e, data) => {
-    if (data) {
-      const response = await chatbotService.handleUsersaySendAgain(
-        bot.id,
-        JSON.parse(data),
-        usersay,
-        client,
-      );
-      res.send({ status: 1, result: response });
-    } else {
-      next();
-    }
-  });
-};
+// const checkCacheUsersay = (req, res, next) => {
+//   const { bot } = req;
+//   const { usersay } = req.query;
+//   // client.lpushAsync();
+//   client.get(bot.id, async (e, data) => {
+//     if (data) {
+//       const response = await chatbotService.handleUsersaySendAgain(
+//         bot.id,
+//         JSON.parse(data),
+//         usersay,
+//         client,
+//       );
+//       res.send({ status: 1, result: response });
+//     } else {
+//       next();
+//     }
+//   });
+// };
 
-module.exports = {
-  checkCacheUsersay: asyncMiddleware(checkCacheUsersay),
-};
+// module.exports = {
+//   checkCacheUsersay: asyncMiddleware(checkCacheUsersay),
+// };
